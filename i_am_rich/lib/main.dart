faa2973b4d6e474d3ada350b4b1088a21ad5b54c
Samarth Gupta <samarthgupta1202@gmail.com>
2023-03-30 18:09:50 +0530

    padding added

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -6,13 +6,18 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
+          centerTitle: true,
           title: const Text("I Am Rich"),
           backgroundColor: Colors.blueGrey[900],
         ),
         body: const Center(
-          child: Image(
-            image: NetworkImage(
-                "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"),
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Image(
+              //child is what is going to be centered
+              image: NetworkImage(
+                  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"),
+            ),
           ),
         ),
       ),
